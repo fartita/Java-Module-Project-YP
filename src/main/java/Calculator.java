c53bbe7c8f2c85c5662563ca07b1d3bd70bce445
Fartita Jehela <varvaran96@yandex.ru>
2024-03-31 13:02:09 +0300

    pre-checking

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Calculator {
+    ArrayList<Pair<String, Double>> listItems = new ArrayList<Pair<String, Double>>();
+
+    public void printResult(int guests){
+        Double allPrice = 0.0;
+        System.out.println("Добавленные товары:\n");
+        for(int i = 0; i < listItems.size(); i++){
+            System.out.println(listItems.get(i).getLeft()+"\n");
+            allPrice = listItems.get(i).getRight() + allPrice;
+        }
+        Double sumPerPerson = allPrice/guests;
+        System.out.printf("Каждый заплатит %.2f%n", sumPerPerson);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int guests = getGuests(scanner);
+        Calculator calc = new Calculator();
+        while(true){
+            System.out.println("Введите товар");
+            String name = scanner.next();
+            System.out.println("Введите стоимость");
+            Double cost  = scanner.nextDouble();
+            calc.listItems.add(new Pair(name, cost));
+            System.out.println("Товар успешно добавлен, завершить подсчет?");
+            String answer = scanner.next();
+            if(answer.toLowerCase().contains("завершить")){
+                break;
+            }
+        }
+        calc.printResult(guests);
+        scanner.close();
     }
+
+    public static int getGuests(Scanner scanner){
+        int count = -1;
+        while(true){
+            System.out.println("Сколько гостей?");
+            count = scanner.nextInt();
+            if(count <= 1){
+                System.out.println("Некорректное значение для подсчета");
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+
 }
